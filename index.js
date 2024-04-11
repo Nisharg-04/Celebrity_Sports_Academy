cf23b8595522955fd9674c2e982ee271d1a1a4c5
dakshilgorasiya <dakshilgorasiya2005@gmail.com>
2024-04-11 18:31:03 +0530

    Add javascript code in index.js instad of jQuery code

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // const nav = document.getElementsByClassName("nav");
-// window.onscroll = function () { 
+// window.onscroll = function () {
 //     if (document.body.scrollTop ) {
 //         nav.classList.add("scrolled");
 //         // nav.classList.remove("nav");
-//     } 
+//     }
 //     else {
 //         // nav.classList.add("nav");
-//         nav.classList.remove("scrolled"); 
+//         nav.classList.remove("scrolled");
 //     }
 // };
-$(function () {
-    $(document).scroll(function () {
-      var $nav = $(".nav");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+// $(function () {
+//     $(document).scroll(function () {
+//       var $nav = $(".nav");
+//       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//     });
+//   });
+window.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        var nav = document.querySelector(".nav");
+        nav.classList.toggle("scrolled", window.scrollY > nav.offsetHeight);
     });
-  });
+});
